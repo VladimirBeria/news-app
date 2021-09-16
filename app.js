52561f68ed80dc1739b90cb9f631fbc5caf606c7
VladimirBeria <volodymyr.dechtiaruk@gmail.com>
2021-09-16 13:47:42 +0300

    adding news to html

--- app.js
+++ app.js
@@ -91,5 +91,31 @@ function onGetResponse(err, res) {
 //function render news
 function renderNews(news) {
     const newsContainer = document.querySelector('.news-container .row')
+    let fragment = ''
 
+    news.forEach(newsItem => {
+        const el = newsTemplate(newsItem)
+        fragment += el
+    })
+    newsContainer.insertAdjacentHTML('afterbegin', fragment)
+}
+
+//News item template function
+function newsTemplate({urlToImage, title, url, description}) {
+    return `
+    <div class="col s12">
+        <div class="card">
+            <div class="card-image">
+                <img src="${urlToImage}">
+                <span class="card-title">${title || ''}</span>
+            </div>
+            <div class="card-content">
+                <p>${description || ''}</p>
+            </div>
+            <div class="card-action">
+                <a href="${url}">Read more</a>
+            </div>
+        </div>
+    </div>
+    `;
 }
